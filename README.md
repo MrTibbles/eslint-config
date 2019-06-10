8e5edfcfcbf70c1ce478e3afe9bac33eb47279b1
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-06-10 11:11:33 +0100

    Add react-rules config file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Packages
 - `@raddad/esling-config` - base default (browser) configuration
+- `@raddad/esling-config/react-rules` - React rule definitions
---
+++ react-rules.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'rules': {
+    "jsx-quotes": ["error", "prefer-double"],
+    "react/display-name": "off",
+    "react/jsx-no-duplicate-props": "error",
+    "react/jsx-no-undef": "error",
+    "react/jsx-sort-props": "warn",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/no-danger": "error",
+    "react/no-deprecated": "error",
+    "react/no-did-mount-set-state": "warn",
+    "react/no-direct-mutation-state": "error",
+    "react/no-find-dom-node": "error",
+    "react/no-unknown-property": "error",
+    "react/no-render-return-value": "error",
+    "react/prop-types": "warn",
+    "react/react-in-jsx-scope": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+}
